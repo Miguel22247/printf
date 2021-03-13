4742e7c269668386ee155d41c961610f0f262ae8
Miguel22247 <minipachru@gmail.com>
2021-03-13 13:52:12 -0300

    nothing added

--- _printf.c
+++ _printf.c
@@ -2,5 +2,9 @@
 /**
  * _printf - is a function that prints
  * @format: gives the format to the function
+ * cases - d, i, s, c, %
  */
-int _printf(const char *format, ...)x
+int _printf(const char *format, ...);
+{
+        
+}

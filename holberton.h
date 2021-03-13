598ef55e3b819cfb9449b05fd71d605f0b1fec5c
Miguel22247 <minipachru@gmail.com>
2021-03-13 17:17:41 -0300

    Add libraries

--- holberton.h
+++ holberton.h
@@ -1,8 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/* libraries to use */
+#include <unistd.h>
 #include <stdlib.h>
-
+#include <stdarg.h>
+#include <stddef.h>
 
 /* Prototypes */
 int _putchar(char c);

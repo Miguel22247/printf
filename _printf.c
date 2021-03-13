ae33791f981d080e084ce73b8d5201d37d9f7796
jtusetganiello <george.zero@gmail.com>
2021-03-13 18:34:51 -0300

    Test to see multiple commits

--- _printf.c
+++ _printf.c
@@ -3,8 +3,9 @@
  * _printf - is a function that prints
  * @format: gives the format to the function
  * cases - d, i, s, c, %
+ * Return: a string.
  */
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
-        
+
 }

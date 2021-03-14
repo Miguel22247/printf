194e54d210d2d4f6acb201290e61d2be060b63f7
Miguel22247 <minipachru@gmail.com>
2021-03-14 10:26:12 -0300

    added print % sign

--- _printf.c
+++ _printf.c
@@ -7,22 +7,24 @@
  */
 int _printf(const char *format, ...)
 {
-	int i;
+        int i;
 /* in case that doesn't contain nothing == NULL*/
-	if (format == NULL)
-		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == 'c')
-				_putchar(format[i + 1]);
-			if (format[i + 1] == 's')
-				_putchar(format[i + 1]);
+        if (format == NULL)
+                return (-1);
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] == '%')
+                {
+                        if (format[i + 1] == '%')
+                                _putchar (format[i]);
+                        if (format[i + 1] == 'c')
+                                _putchar(format[i + 1]);
+                        if (format[i + 1] == 's')
+                                _putchar(format[i + 1]);
 /*string magical shenanigans happen here */
-				i = i + 2;
-		}
-		_putchar(format[i]);
-	}
-	return (*format);
+                                i = i + 2;
+                }
+                _putchar(format[i]);
+        }
+        return (*format);
 }

98bd2d00ea12a24dc64606565047ca0f22c2fa41
Miguel22247 <minipachru@gmail.com>
2021-03-14 17:33:07 -0300

    added print_d function

--- holberton.h
+++ holberton.h
@@ -20,5 +20,6 @@ int _printf(const char *format, ...);
 int print_c(va_list list);
 int print_s(va_list list);
 int print_i(va_list list);
+int print_d(va_list list);
 int print_mod(va_list list);
 #endif

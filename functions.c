ba7c8404d6d2201bf050848bdd664feabefb6d0c
jtusetganiello <george.zero@gmail.com>
2021-03-15 15:06:39 -0300

    Bifurcation

--- functions.c
+++ functions.c
@@ -1,48 +1,5 @@
 #include "holberton.h"
 /**
- * print_c - print a character
- * @list: list
- * Return: a character
- */
-
-int print_c(va_list list)
-{
-	char *c = va_arg(list, char *);
-	write(1, &c, 1);
-	va_end(list);
-	return (1);
-}
-
-/**
- * print_s - prints a string
- * @list: list
- * Return: a string
- */
-
-int print_s(va_list list)
-{
-	char *str = va_arg(list, char *);
-	int len = strlen(str);
-	write(1, str, len);
-	va_end(list);
-	return (len);
-}
-
-/**
- * print_mod - print a '%' sign
- * @list: list
- * Return: a '%' sign
- */
-
-int print_mod(va_list list)
-{
-	char c = '%';
-	write(1, &c, 1);
-	va_end(list);
-	return (1);
-}
-
-/**
  * printnum - Helper function for counting
  * @n: number that gets added recursively
  * Return: void
@@ -100,11 +57,19 @@ int print_d(va_list list)
 	count = printnum(output);
 	return (count);
 }
+
+/**
+ * print_r - prints a reverse?
+ * @list: list
+ * Return: stuff!
+ */
+
 int print_r(va_list list)
 {
 	char *str = "%r";
-       int len = strlen(str);
-        write(1, str, len);
-        va_end(list);
-        return (len);
+	int len = strlen(str);
+
+	_putchar(*str);
+	va_end(list);
+	return (len);
 }

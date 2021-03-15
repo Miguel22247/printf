ff366088d783e55df6e52f340cc79a8a7eb7448c
Miguel22247 <minipachru@gmail.com>
2021-03-15 22:06:54 +0000

    removed NULL condition in string

--- functions2.c
+++ functions2.c
@@ -32,17 +32,9 @@ int print_s(va_list list)
 	char *str = va_arg(list, char *);
 	int len = _strlen(str);
 
-	if (str == NULL)
-	{
-		str = NULL;
-		return (-1);
-	}
-	else
-	{
 		write(1, str, len);
 		va_end(list);
 		return (len);
-	}
 }
 
 /**

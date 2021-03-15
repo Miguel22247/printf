b440fda898c545859832e5aca4a7685cf9493349
Miguel22247 <minipachru@gmail.com>
2021-03-15 14:35:15 +0000

    added and commented printf %r

--- test/main.c
+++ test/main.c
@@ -38,5 +38,7 @@ len = _printf("Percent:[%%]\n");
 len2 = printf("Percent:[%%]\n");
 _printf("Len:[%d]\n", len);
 printf("Len:[%d]\n", len2);
+/* _printf("Unknown:[%r]\n"); */
+/* printf("Unknown:[%r]\n"); */
 return (0);
 }

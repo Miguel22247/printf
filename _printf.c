c5f92d43540ca9d71f990ac996c193410abcbae4
Miguel Pacheco <minipachru@gmail.com>
2021-03-16 11:09:39 -0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,47 +7,44 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int length = 0;
-	int i = 0, j;
+        va_list ap;
+        int length = 0;
+        int i = 0, j;
 
-	pr_f ops[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"%", print_mod},
-		{"d", print_d},
-		{"i", print_d},
-		{"r", print_r},
-		{NULL, NULL}
-	};
-	if (!format)
-		return (-1);
-	va_start(ap, format);
-	for (i = 0; format [i] != '\0'; i++)
-	{
-		if (format[i] == '%')
+        pr_f ops[] = {
+                {"c", print_c},
+                {"s", print_s},
+                {"%", print_mod},
+                {"d", print_d},
+                {"i", print_d},
+                {"r", print_r},
+                {NULL, NULL}
+        };
+        va_start(ap, format);
+        if (!format)
+                return (-1);
+        while (format != NULL && format[i] != '\0')
+        {
+                j = 0;
+                if (format[i] == '%')
 		{
-			i++;
-			if (format[i] == '\0')
-				return (-1);
-			if (format[i] == '%')
-			{
-				_putchar(format[i]);
-				length++;
-			}
-			for (j = 0; (ops + j)-> op != NULL; j++)
-			{
-				if (format[i] == *(ops + j)->op)
-					length += (ops + j)->f(ap);
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			length++;
-		}
-	}
-	va_end(ap);
-	return (length);
-
+ 			i += 1;
+                        while (ops[j].op != NULL)
+                        {
+                                if (format[i] == ops[j].op[0])
+                                {
+                                        length += ops[j].f(ap);
+                                        break;
+                                }
+                                j++;
+                        }
+                }
+                else
+                {
+                        length += _putchar(format[i]);
+                }
+                i++;
+        }
+        va_end(ap);
+        return (length);
 }
